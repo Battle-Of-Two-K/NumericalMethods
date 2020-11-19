e71a88d7906826c631dc5cbda817244d570c6f7d
Battle-Of-Two-K <tr.penrose@mail.ru>
2020-11-19 08:39:17 +0300

    Метод конечных разностей

--- Battle_Of_Two_K/Task_13_by_BOF2K.py
+++ Battle_Of_Two_K/Task_13_by_BOF2K.py
@@ -1,222 +1,285 @@
+import matplotlib.pyplot as plt
 from python_code.main import *
 from sympy.abc import x
+import numpy as np
 
-# Ввод исходных данных:
-print("\n                             ___Тема: решение краевой задачи для ОДУ___")
-print("                             _________Метод конечных разностей_________\n")
-print("-" * 100)
-print("*Примечание: дан отрезок [a, b], дано уравнение вида K(x)*y'' +L(x)*y' + M(x)*y = F(x), а также даны")
-print("краевые (граничные) условия 3-го рода на границах отрезка: R*y'(a) + S*y(a) = T и V*y'(b) + W*y(b) = Z")
-print("                          небольшая подсказка: R и V во всех вариантах равны 0")
-print("-" * 100)
-print("* - умножение")
-print("** - возведение в степень")
-print("6.5 - число с плавающей точкой (да, ставить нужно именно ТОЧКУ, а не запятую)")
-print("-" * 100)
-print("Введите свои данные (если не понятно, что вводить, но смотрите примечание выше или же читайте методичку):\n")
-# a = input("a = ")
-# b = input("b = ")
-# h = input("h = ")
-# K = input("K(x) = ")
-# L = input("L(x) = ")
-# M = input("M(x) = ")
-# F = input("F(x) = ")
-# R = input("R = ")
-# S = input("S = ")
-# T = input("T = ")
-# V = input("V = ")
-# W = input("W = ")
-# Z = input("Z = ")
+try:
+    # Ввод исходных данных:
+    print("\n                             ___Тема: решение краевой задачи для ОДУ___")
+    print("                             _________Метод конечных разностей_________\n")
+    print("-" * 100)
+    print("*Примечание: дан отрезок [a, b], дано уравнение вида K(x)*y'' +L(x)*y' + M(x)*y = F(x), а также даны")
+    print("краевые (граничные) условия 3-го рода на границах отрезка: R*y'(a) + S*y(a) = T и V*y'(b) + W*y(b) = Z")
+    print("-" * 100)
+    print("* - умножение")
+    print("** - возведение в степень")
+    print("-" * 100)
+    print()
 
-# Вариант из методички
-# a = 1
-# b = 10
-# h = 9 / 4
-# K = 2 * x ** 2
-# L = x
-# M = 1
-# F = 2 * x ** .5
-# R = 0
-# S = 1
-# T = 2
-# V = 0
-# W = 1
-# Z = 2 * 10 * 0.5
+    # Вариант из методички (работает, можно проверить)
+    a = 1
+    b = 10
+    h = 9 / 4
+    K = 2 * x ** 2
+    L = x
+    M = 1
+    F = 2 * x ** .5
+    R = 0
+    S = 1
+    T = 2
+    V = 0
+    W = 1
+    Z = 2 * 10 ** 0.5
 
-# Вариант Владоса
-# a = 1
-# b = 2
-# h = 0.2
-# K = 1
-# L = -1
-# M = 3
-# F = -x ** 2 - 4 * x + 2
-# R = 0
-# S = 1
-# T = 2
-# V = 0
-# W = 2
-# Z = 5
+    # Мой вариант (вариант 7):
+    # a = -3
+    # b = -2
+    # h = 0.2
+    # K = 2
+    # L = -1
+    # M = 4
+    # F = -(x ** 2) - 3 * x + 5
+    # R = 0
+    # S = -4
+    # T = 1
+    # V = 0
+    # W = -4
+    # Z = 5
 
-# Вариант Паши
-a = -2
-b = -1
-h = 0.2
-K = 1
-L = -2
-M = 1
-F = -x * x - x + 1
-R = 0
-S = -3
-T = -4
-V = 0
-W = 3
-Z = -4
+    # Вариант Паши
+    # a = -2
+    # b = -1
+    # h = 0.2
+    # K = 1
+    # L = -2
+    # M = 1
+    # F = -(x ** 2) - x + 1
+    # R = 0
+    # S = -3
+    # T = -4
+    # V = 0
+    # W = 3
+    # Z = -4
 
-# -------------------------------------------
-# Предупреждение!!! Ниже программный код!!!
-# -------------------------------------------
+    # Вариант Влада:
+    # a = 1
+    # b = 2
+    # h = 0.2
+    # K = 1
+    # L = -1
+    # M = 3
+    # F = -(x ** 2) - 4 * x + 2
+    # R = 0
+    # S = 1
+    # T = 2
+    # V = 0
+    # W = 2
+    # Z = 5
 
-y_a = T / S
-y_b = Z / W
+    # -------------------------------------------
+    # Предупреждение!!! Ниже программный код!!!
+    # -------------------------------------------
 
-# Первая итерация
-b_1 = -R / h + S
-c_1 = R / h
-d_1 = T
+    y_a = T / S
+    y_b = Z / W
 
-# Последняя итерация
-a_n = V / h
-b_n = -V / h + W
-d_n = -Z
+    # Первая итерация
+    b_1 = -R / h + S
+    c_1 = R / h
+    d_1 = T
 
+    # Последняя итерация
+    a_n = V / h
+    b_n = -V / h - W
+    d_n = -Z
 
-def number_of_segments():
-    """
-    Returns:
-        int: возвращает количество отрезков одинаковой длины
-    """
-    return int((b - a) / h)
+    def del_zero(value: any):
+        """
+        Удаление незначащих нулей после запятой + округление до 9 знака.
 
+        Args:
+            value: любое число (НО не str -> TypeError: must be real number, not str)
 
-def all_x():
-    """
-    Returns:
-        list: возвращает список всех значений x от a до b
-    """
-    output = a
-    list_x = []
-    while output <= b:
-        list_x.append(output)
-        output += h
-    return list_x
+        Returns:
+            число без лишних нулей (или же округлённое число)
 
+        Examples:
+            * def_zero(45.400000000000) -> 45.4
+            * def_zero(3.3999999999994) -> 3.4
+            * def_zero(2.4500000000004) -> 2.45
+            * def_zero(2.0000004) -> 2
+            * def_zero(2.000004) -> 2.000004
 
-def all_a():
-    """
-    Returns:
-        list: возвращет список всех значений a_i
-    """
-    list_a = []
-    for step in all_x():
-        new_a = parse_expr(str(K)) / (h ** 2) - parse_expr(str(L)) / (2 * h)
-        list_a.append(new_a)
-    list_a.pop(0)
-    n = len(list_a) - 1
-    list_a.pop(n)
-    list_a.insert(n, a_n)
-    return list_a
+        Из вышеприведённого примера, можно заметить, что округление происходит
+        после того, как кол-во знаков после плавающей точки становиться равным 7 и более,
+        при меньшем кол-ве округления не происходит.
+        """
+        return float(('%.9f' % value).rstrip('0').rstrip('.'))
 
+    def rounded_list(new_list: list):
+        """
+        Округление элементов списка.
 
-def all_b():
-    """
-    Returns:
-        list: возвращает спсиок всех значений b_i
-    """
-    list_b = []
-    for step in all_x():
-        new_a = -2 * parse_expr(str(K)) / (h ** 2) + parse_expr(str(M))
-        list_b.append(new_a)
-    list_b.pop(0)
-    list_b.insert(0, b_1)
-    n = len(list_b) - 1
-    list_b.pop(n)
-    list_b.insert(n, b_n)
-    return list_b
+        Args:
+            new_list: любой список с числами
+
+        Returns:
+            Список с округлёнными элементами
+
+        """
+        output_list = []
+        for step in new_list:
+            output_list.append(del_zero(step))
+        return output_list
+
+    def number_of_segments():
+        """
+        Returns:
+            int: возвращает количество отрезков одинаковой длины
+        """
+        return int((b - a) / h)
 
 
-def all_c():
-    """
-    Returns:
-        list: возвращет список всех значений с_i
-    """
-    list_c = []
-    for step in all_x():
-        new_a = parse_expr(str(K)) / (h ** 2) + parse_expr(str(L)) / (2 * h)
-        list_c.append(new_a)
-    list_c.pop(0)
-    list_c.insert(0, c_1)
-    n = len(list_c) - 1
-    list_c.pop(n)
-    return list_c
+    def all_x():
+        """
+        Returns:
+            list: возвращает список всех значений x от a до b
+        """
+        output = a
+        list_x = []
+        while output <= b:
+            list_x.append(output)
+            output += h
+        return rounded_list(list_x)
 
 
-def all_d():
-    """
-    Returns:
-        list: возвращет список всех значений d_i
-              (он же столбец свободных членов)
-    """
-    list_d = []
-    for step in all_x():
-        new_a = F.evalf(subs={x: step})
-        list_d.append(new_a)
-    return list_d
+    def all_a():
+        """
+        Returns:
+            list: возвращет список всех значений a_i
+        """
+        list_a = []
+        for step in all_x():
+            new_a = parse_expr(str(K)).evalf(subs={x: step}) / (h ** 2) -\
+                    parse_expr(str(L)).evalf(subs={x: step}) / (2 * h)
+            list_a.append(new_a)
+        list_a.pop(0)
+        n = len(list_a) - 1
+        list_a.pop(n)
+        list_a.insert(n, a_n)
+        return rounded_list(list_a)
 
 
-def fill_triple_from_lists(list_up: list, list_middle: list, list_down: list) -> Matrix:
-    """
-    Заполняет трехдиагональную матрицу, используя 3 списка
-
-    Args:
-        list_up (list): список над главной диагональю
-        list_middle (list): список главной диагонали
-        list_down (list): список под главной диагональю
-
-    Returns:
-        Matrix: заполненная трехдиагональная матрица
-    """
-    out_matrix = Matrix(len(list_middle))
-
-    for row_no, col_no in out_matrix:
-        if row_no == col_no - 1:
-            out_matrix[row_no][col_no] = list_up[col_no - 1]
-        if row_no == col_no:
-            out_matrix[row_no][col_no] = list_middle[row_no]
-        if row_no - 1 == col_no:
-            out_matrix[row_no][col_no] = list_down[row_no - 1]
-
-    return out_matrix
+    def all_b():
+        """
+        Returns:
+            list: возвращает спсиок всех значений b_i
+        """
+        list_b = []
+        for step in all_x():
+            new_a = -2 * parse_expr(str(K)).evalf(subs={x: step}) /\
+                    (h ** 2) + parse_expr(str(M)).evalf(subs={x: step})
+            list_b.append(new_a)
+        list_b.pop(0)
+        list_b.insert(0, b_1)
+        n = len(list_b) - 1
+        list_b.pop(n)
+        list_b.insert(n, b_n)
+        return rounded_list(list_b)
 
 
-def progonka():
-    matrix = fill_triple_from_lists(
-        all_c(),
-        all_b(),
-        all_a(),
-    )
+    def all_c():
+        """
+        Returns:
+            list: возвращет список всех значений с_i
+        """
+        list_c = []
+        for step in all_x():
+            new_a = parse_expr(str(K)).evalf(subs={x: step}) / (h ** 2) +\
+                    parse_expr(str(L)).evalf(subs={x: step}) / (2 * h)
+            list_c.append(new_a)
+        list_c.pop(0)
+        list_c.insert(0, c_1)
+        n = len(list_c) - 1
+        list_c.pop(n)
+        return rounded_list(list_c)
 
-    matrix.console_display()
 
-    free_column = all_d()
-    decision = iterations.triple_diagonal(matrix, free_column, level_of_detail=2)
+    def all_d():
+        """
+        Returns:
+            list: возвращет список всех значений d_i
+                  (он же столбец свободных членов)
+        """
+        list_d = []
+        for step in all_x():
+            new_a = parse_expr(str(F)).evalf(subs={x: step})
+            list_d.append(new_a)
+        list_d.pop()
+        list_d.insert(len(list_d), d_n)
+        list_d.pop(0)
+        list_d.insert(0, d_1)
+        return rounded_list(list_d)
 
-    solution = None
-    for step in decision:
-        solution = step.get("Решение")
-        print(step)
+
+    def fill_triple_from_lists(list_up: list, list_middle: list, list_down: list) -> Matrix:
+        """
+        Заполняет трехдиагональную матрицу, используя 3 списка
+
+        Args:
+            list_up (list): список над главной диагональю
+            list_middle (list): список главной диагонали
+            list_down (list): список под главной диагональю
+
+        Returns:
+            Matrix: заполненная трехдиагональная матрица
+        """
+        out_matrix = Matrix(len(list_middle))
+
+        for row_no, col_no in out_matrix:
+            if row_no == col_no - 1:
+                out_matrix[row_no][col_no] = list_up[col_no - 1]
+            if row_no == col_no:
+                out_matrix[row_no][col_no] = list_middle[row_no]
+            if row_no - 1 == col_no:
+                out_matrix[row_no][col_no] = list_down[row_no - 1]
+
+        return out_matrix
 
 
-print(y_a)
-print(y_b)
-progonka()
+    def main():
+        matrix = fill_triple_from_lists(
+            all_c(),
+            all_b(),
+            all_a(),
+        )
+
+        print(f'Столбец свободных членов {all_d()}')
+        print(matrix.to_pretty_string())
+
+        free_column = all_d()
+        decision = iterations.triple_diagonal(matrix, free_column, level_of_detail=3)
+
+        solution = None
+        for step in decision:
+            solution = step.get("Решение")
+            print(step)
+
+        # \\\\\\\\\\\\\\\\\\\\\\
+        # Красивый вывод графика
+        # \\\\\\\\\\\\\\\\\\\\\\
+
+        # TODO: Допилить график!
+        # X = np.linspace(-10, 10, 100)
+        # Y = F
+        # figure, axes = plt.subplots()
+        # axes.plot(X, Y, color='Navy')
+        # axes.grid()
+        # axes.scatter(all_x(), [-0.25, -0.7430795371684206, -1.1084344330140712, -1.291747997221237, -1.25], color='red')
+        # axes.set_title('Метод конечных разностей')
+
+    main()
+
+except Exception as error:
+    print(error)
+input('\nНажмите "Enter" чтобы выйти...')
--- Battle_Of_Two_K/test_MNK.py
+++ Battle_Of_Two_K/test_MNK.py
@@ -1,28 +1,116 @@
-import pytest
-from Battle_Of_Two_K.MNK import amount_elem, sum_x, square_sum_x, cubic_sum_x, fourth_degree_sum_x, sum_y, sum_x_y, \
-    square_x_sum_y
+from sympy import symbols
 
-X = [-6, -5, -4, -2]
-Y = [30, 22, 16, 6]
+x, y = symbols('x  y ')
+
+A = -2  # Print
+B = -1  # Print
+
+# Считаем колличество шагов
+h = 0.2
+Go = int((B - A) / h)  # 4
+# Go =5
+#
+K = 1
+L = -2
+M = 1
+
+Fx = -1 * (x ** 2) - x + 1
+
+# Print
+R = 0
+S = -3
+T = - 4
+#
+V = 0
+W = 3
+Z = -4
+#
+Xx = [A]
+for i in range(Go):
+    Xx.append(Xx[i] + h)
 
 
-def test_MNK():
-    a = amount_elem()
-    b = sum_x()
-    c = square_sum_x()
-    d = cubic_sum_x()
-    e = fourth_degree_sum_x()
-    f = sum_y()
-    g = sum_x_y()
-    h = square_x_sum_y()
-    assert a == 4
-    assert b == -17
-    assert c == 81
-    assert d == -413
-    assert e == 2193
-    assert f == 74
-    assert g == -366
-    assert h == 1910
+# Создаем матрицу
+def Matrix():
+    global Mat1
+    # print(Go)
+    A = K / h ** 2 - L / 2 / h
+    B = (-2) * K / h ** 2 + M
+    C = K / h ** 2 + L / 2 / h
+    print('b1=', R / h + S)
+    print('c1=', R / h)
+    print('d1=', Fx.evalf(subs={x: Xx[1]}))
+    print('A', A)
+    print('B', B)
+    print('C', C)
+    print('d2=', Fx.evalf(subs={x: Xx[1]}))
+    print('d3=', Fx.evalf(subs={x: Xx[2]}))
+    print('d4=', Fx.evalf(subs={x: Xx[3]}))
+    print('d5=', Fx.evalf(subs={x: Xx[4]}))
+    print('a6=', V / h)
+    print('b6=', (-V / h - W))
+    print('d6=', -Z, '\n')
+
+    Mat1 = [[R / h + S, R / h, 0, 0, 0, 0, T],
+            [A, B, C, 0, 0, 0, Fx.evalf(subs={x: Xx[1]})],
+            [0, A, B, C, 0, 0, Fx.evalf(subs={x: Xx[2]})],
+            [0, 0, A, B, C, 0, Fx.evalf(subs={x: Xx[3]})],
+            [0, 0, 0, A, B, C, Fx.evalf(subs={x: Xx[4]})],
+            [0, 0, 0, 0, V / h, (-V / h - W), -Z]]
 
 
-test_MNK()
+print('-' * 10, 'Расчеты', '-' * 10)
+
+Matrix()
+
+for row in Mat1:
+    for list in row:
+        print("{:>13.8f}".format(list), end=' ')
+    print()
+
+print(f'\n X = {Xx}')
+
+
+# Progonka
+def P():
+    Pn = [(-Mat1[0][1] / Mat1[0][0])]
+    for i in range(1, len(Mat1) - 1):
+        S = (-Mat1[i][i + 1]) / (Mat1[i][i] + Mat1[i][i - 1] * Pn[i - 1])
+        Pn.append(S)
+    Pn.append(0)
+    return Pn
+
+
+def Q():
+    Qn = [(Mat1[0][len(Mat1)] / Mat1[0][0])]
+    for i in range(1, len(Mat1)):
+        S = (Mat1[i][len(Mat1)] - Mat1[i][i - 1] * Qn[i - 1]) / (Mat1[i][i] + Mat1[i][i - 1] * P()[i - 1])
+        Qn.append(S)
+    return Qn
+
+
+def End():
+    global Zn
+    Zn = [Q()[len(Mat1) - 1]]
+    S = Zn[len(Zn) - 1]
+    for i in range(len(Mat1) - 2, -1, -1):
+        Z = Q()[i] + P()[i] * S
+        S = Z
+        Zn.append(Z)
+    Zn.reverse()
+    return Zn
+
+
+print('\n', 'P', P())
+print('\n', 'Q', Q())
+print('\n', 'Y=1,2....', '\n', End())
+
+print('-' * 10, 'Ответ', '-' * 10)
+print('\nX=', end=' ')
+for row in Xx:
+    print("{:>13.8f}".format(row), end=' ')
+print('\nY=', end=' ')
+for row in Zn:
+    print("{:>13.8f}".format(row), end=' ')
+
+input()
