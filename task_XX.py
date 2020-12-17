c8b7901deeee92d863a22bc0ec3f8d2354cd724f
simensgreen <simensgreen007@gmail.com>
2020-12-18 02:42:38 +0300

    Уточнение по формуле Рунге-Роберта

--- task_XX.py
+++ task_XX.py
@@ -1,17 +1,20 @@
-from python_code.methods.integrate.trapezoid import trapezoid_integral
+from python_code.main import Matrix
+from python_code.methods.integrate.runge_check import runge_check
 from python_code.methods.integrate.simpson import simpson_integral
+from python_code.methods.integrate.trapezoid import trapezoid_integral
 from python_code.staf.sympy_init import *
 
 
-# ============================================
-# Численное интегрирование по формуле трапеций
-# ============================================
+# ====================================================
+#  Численное  интегрирование  по  формуле  трапеций,
+# формула Симпсона, уточнение по формуле Рунге-Роберта
+# ====================================================
 
 
 def main():
     function = 'sin(x)'  # интегрируемая функция
     section = (0, pi / 2)  # пределы интегрирования
-    number_of_steps = 4  # количество шагов
+    numbers_of_steps = 4, 8, 16  # количество шагов
 
     # ============================================================
     # ВНИМАНИЕ! Пугливым ниже не смотреть! Дальше программный код!
@@ -25,16 +28,43 @@ def main():
                 step_info += f'{info}: {list(map(lambda val: round(float(val), 8), step[info]))}\n'
             elif isinstance(data[info], float):
                 step_info += f'{info}: {round(data[info], 8)}\n'
+            elif isinstance(data[info], Matrix):
+                print(info)
+                data[info].map(float).console_display()
+                continue
             else:
                 step_info += f'{info}: {data[info]}\n'
         print(step_info)
 
     print(' Численное интегрирование по формуле трапеций '.center(75, '='))
-    decision = trapezoid_integral(function, section, number_of_steps, level_of_details=1)
+    for_check_steps = []
+    for_check_results = []
+    for steps_num in numbers_of_steps:
+        decision = trapezoid_integral(function, section, steps_num, level_of_details=1)
+        for step in decision:
+            if 'Величина шага (h)' in step:
+                for_check_steps.append(step['Величина шага (h)'])
+            if 'Решение' in step:
+                for_check_results.append(step['Решение'])
+            print_data(step)
+    print(' Уточнение полученных результатов по формуле Рунге-Роберта '.center(75, '='))
+    decision = runge_check(for_check_results, for_check_steps, level_of_details=2)
     for step in decision:
         print_data(step)
 
-    decision = simpson_integral(function, section, number_of_steps, level_of_details=1)
+    print(' Численное интегрирование по формуле Симпсона '.center(75, '='))
+    for_check_steps = []
+    for_check_results = []
+    for steps_num in numbers_of_steps:
+        decision = simpson_integral(function, section, steps_num, level_of_details=1)
+        for step in decision:
+            if 'Величина шага (h)' in step:
+                for_check_steps.append(step['Величина шага (h)'])
+            if 'Решение' in step:
+                for_check_results.append(step['Решение'])
+            print_data(step)
+    print(' Уточнение полученных результатов по формуле Рунге-Роберта '.center(75, '='))
+    decision = runge_check(for_check_results, for_check_steps, level_of_details=2)
     for step in decision:
         print_data(step)
 
