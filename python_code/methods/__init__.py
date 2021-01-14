5dc88eb1eb2329eeebb0d2b55e6f7fedc91a8813
simensgreen <simensgreen007@gmail.com>
2021-01-14 15:33:48 +0300

    Удалена папка python_code

--- python_code/methods/__init__.py
+++
@@ -1,2 +0,0 @@
-import python_code.methods.matrix as matrix
-import python_code.methods.equation as equation
--- python_code/methods/approx/linear_approx.py
+++
@@ -1,3 +0,0 @@
-def linear_approx(table, level_of_details=3):
-    # TODO: Аппроксимация методом наименьших квадратов (линейная)
-    pass
